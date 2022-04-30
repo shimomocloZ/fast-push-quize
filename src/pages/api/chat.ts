3051400f473e04cdb350fc3cdc0e76f8ecbdb892
shimomocloZ <shimomoclo.sys@gmail.com>
2022-04-30 19:46:06 +0900

    多分Vercelだとエラーになるだろうな、、

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -5,7 +5,7 @@ const handle = (req: NextApiRequest, res: NextApiResponseServerIO) => {
   if (req.method === `POST`) {
     const message = req.body
     // messageイベントの実行
-    res?.socket?.server?.io?.emit(`update-data`, message)
+    res.socket.server.io.emit(`update-data`, message)
     res.status(201).json(message)
   }
 }
